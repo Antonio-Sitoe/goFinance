0536f425e2816a70b1c8a7cdf80f4332c4252863
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-12-19 16:39:28 +0200

    Update styles.ts

--- src/screens/Dasboard/styles.ts
+++ src/screens/Dasboard/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Paragraph = styled.Text`
   color: whitesmoke;
+  margin-bottom: 2rem;
 `;
 
 export const Button = styled.Button`
