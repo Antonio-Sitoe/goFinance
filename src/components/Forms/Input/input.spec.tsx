f7f7c068c17156a7b130fe06286bfc67aac2b6ef
Antonio Sitoe <antonio.sitoe@void.co.mz>
2023-05-16 17:19:09 +0200

    instalado jest styled-components e mocando contextos

--- src/components/Forms/Input/input.spec.tsx
+++ src/components/Forms/Input/input.spec.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import Input from "./Input";
+import theme from "../../../global/styles/theme";
+import { ThemeProvider } from "styled-components/native";
+
+const Provider = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+// mocamos um contexto
 
 describe("Input Component", () => {
   it("must have border specific border color when active", () => {
-    const { getByTestId } = render(
+    const { getByTestId, debug } = render(
       <Input
         testID="input-email"
         placeholder="E-mail"
         keyboardType="email-address"
         autoCorrect={false}
         active={true}
-      />
+      />,
+      {
+        wrapper: Provider,
+      }
     );
-
+    debug();
     const inputComponent = getByTestId("input-email");
-    expect(inputComponent.props.style[0].borderColor).toEqual("#E83F5B");
+    expect(inputComponent.props.style[0].borderColor).toEqual(
+      theme.colors.attention
+    );
   });
 });
